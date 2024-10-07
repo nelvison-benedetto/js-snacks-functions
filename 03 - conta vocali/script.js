3aa1793ee12066eb7e7b9fbe01e90bebbede1a59
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-07 17:07:25 +0200

    finish 03

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -2,14 +2,25 @@
 
 const word = "javascript";
 const vocals = "aeiou";
+let setvocals = new Set(vocals);
+
+let counts = {};
+const myMap = new Map();
 
 // Dichiara la funzione qui.
 function countVocals(){
-    Array.from(word).forEach( (elem,i) =>   )  //avoid complessity O(n * m) !!
+    Array.from(word).forEach( (item,i) => {
+        counts[item] = (counts[item] || 0) + 1;
+        if (vocals.includes(item.toLowerCase())){   
+            myMap.set(item,counts[item]);
+        }
+    }
+    )
+    return  myMap; 
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(countVocals());
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
